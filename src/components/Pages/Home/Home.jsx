040f8630055a3c7a2f4745db45f848e49019db07
unknown <frontendcnn@gmail.com>
2025-11-20 17:10:12 -0300

    finalizando o responsive do pos2025

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -3,33 +3,35 @@ import styles from './Home.module.css';
 export const Home = () => {
     return (
         <section className={styles.containerBox}>
-            <div className={styles.box}>
-                <p className={styles.title}>
-                    Developed <br />Frontend
-                </p>
-                <p className={styles.pos}>POS 2025</p>
-                <p className={styles.button}>
-                    Explore mais!
-                </p>
-
-
-                <div className={styles.dflex}>
-                    <span>
-                        <i className='fa-solid fa-bars'></i>
-                    </span>
-                    <p>
-                        oi aquie os demais itemss
-
+            <div className='container'>
+                <div className={styles.box}>
+                    <p className={styles.title}>
+                        Developed <br />Frontend
                     </p>
-                </div>
-                <div className={styles.dflex}>
-                    <span>
-                        <i className='fa-solid fa-bars'></i>
-                    </span>
-                    <p>
-                        oi aquie os demais itemss
-
+                    <p className={styles.pos}>POS 2025</p>
+                    <p className={styles.button}>
+                        Explore mais!
                     </p>
+
+
+                    <div className={styles.dflex}>
+                        <span>
+                            <i className='fa-solid fa-bars'></i>
+                        </span>
+                        <p>
+                            oi aquie os demais itemss
+
+                        </p>
+                    </div>
+                    <div className={styles.dflex}>
+                        <span>
+                            <i className='fa-solid fa-bars'></i>
+                        </span>
+                        <p>
+                            oi aquie os demais itemss
+
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
